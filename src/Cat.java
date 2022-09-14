1b1fe6f6cd6e5ab4d63028d53034ab38b0fedbe1
Sarah Beckett <sarah.beckett@sky.uk>
2022-09-14 21:12:42 +0100

    first commit

---
+++ src/Cat.java
@@ -0,0 +1,34 @@
+package src;
+public class Cat{
+    private int numWhiskers;
+    private String colour;
+    public Boolean canRetractClaws;
+
+    public Boolean getCanRetractClaws() {
+        return canRetractClaws;
+    }
+
+    public void setCanRetractClaws(Boolean canRetractClaws) {
+        this.canRetractClaws = canRetractClaws;
+    }
+
+    public String makeNoise(){
+        System.out.println("Purr");
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getNumWhiskers() {
+        return numWhiskers;
+    }
+
+    public void setNumWhiskers(int numWhiskers) {
+        this.numWhiskers = numWhiskers;
+    }
+}
---
+++ src/SmallCat.java
@@ -0,0 +1,9 @@
+package src;
+
+public class SmallCat extends Cat {
+    private Byte numLives;
+    
+    public String makeNoise(){
+        System.out.println("Miaow");
+    
+}
